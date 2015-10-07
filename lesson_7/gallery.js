f015e7694e45887cf47517cac3e9cea9712d9259
unknown <javascript@1-00.itfactory.neta>
2015-10-07 19:53:49 +0200

    lesson 7 gallery refactoring 2

--- lesson_7/gallery.js
+++ lesson_7/gallery.js
@@ -1,55 +1,63 @@
 'use strict';
 
-var imageFiles = [
-  'majom_1.jpg',
-  'majom_2.jpg',
-  'majom_3.jpg',
-  'majom_4.jpg'
-];
+(function () {
+	var imageFiles = [
+	  'majom_1.jpg',
+	  'majom_2.jpg',
+	  'majom_3.jpg',
+	  'majom_4.jpg'
+	];
 
-var imageWrapper, prev, next, currentIndex, img;
+	var prev, next, img, currentIndex;
 
-init();
-document.body.addEventListener('click', function(event) {
-	setCurrentIndexBasedOnDirection(event.target.id);
-	setCurrentImage();
-    disableButtonsOnEnds();
-});
+	init();
+	
+	document.body.addEventListener('click', function(event) {
+		setCurrentIndexBasedOnDirection(event.target.id);
+		displayCurrentImage();
+		disableButtonsOnEnds();
+	});
 
-function init() {
-	imageWrapper = document.getElementById('image-wrapper');
-	prev = document.getElementById('prev');
-	next = document.getElementById('next');
-
-	currentIndex = 0;
-	prev.setAttribute('disabled', 'disabled');
-
-	img = document.createElement('img');
-	setCurrentImage();
-	imageWrapper.appendChild(img);
-}
-
-function setCurrentImage() {
-	img.src = imageFiles[currentIndex];
-}
-
-function setCurrentIndexBasedOnDirection(direction) {
-	if (direction === 'next') {	
-	  currentIndex = Math.min(currentIndex + 1, imageFiles.length - 1);
-	} else if (direction === 'prev') {
-	  currentIndex = Math.max(currentIndex - 1, 0);
+	function init() {
+		initDomElements();
+		currentIndex = 0;
+		displayCurrentImage();
 	}
-}
-
-function disableButtonsOnEnds() {
-	addAttributeOnIndex(prev, 0);
-	addAttributeOnIndex(next, imageFiles.length - 1);
-}
-
-function addAttributeOnIndex(button, index) {
-	if (currentIndex === index) {
-		button.setAttribute('disabled', 'disabled');
-	} else {
-		button.removeAttribute('disabled');
+	
+	function initDomElements() {
+		prev = document.getElementById('prev');
+		prev.setAttribute('disabled', 'disabled');
+		next = document.getElementById('next');
+		img = document.createElement('img');
+		addImageElement();
 	}
-}
+	
+	function addImageElement() {
+		document.getElementById('image-wrapper').appendChild(img);
+	}
+
+	function displayCurrentImage() {
+		img.src = imageFiles[currentIndex];
+	}
+
+	function setCurrentIndexBasedOnDirection(direction) {
+		if (direction === 'next') {	
+		  currentIndex = Math.min(currentIndex + 1, imageFiles.length - 1);
+		} else if (direction === 'prev') {
+		  currentIndex = Math.max(currentIndex - 1, 0);
+		}
+	}
+
+	function disableButtonsOnEnds() {
+		addAttributeOnIndex(prev, 0);
+		addAttributeOnIndex(next, imageFiles.length - 1);
+	}
+
+	function addAttributeOnIndex(button, index) {
+		if (currentIndex === index) {
+			button.setAttribute('disabled', 'disabled');
+		} else {
+			button.removeAttribute('disabled');
+		}
+	}
+})();
