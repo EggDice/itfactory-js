f15c210cd6c99f588d4687d88cb275e9fca8b2bc
unknown <javascript@4-00.itfactory.neta>
2015-10-13 19:54:42 +0200

    game

--- lesson_9/index.js
+++ lesson_9/index.js
@@ -5,12 +5,26 @@ var fs = require('fs');
 
 var app = express();
 
+var number = 42;
+var lastPlayer = '';
+
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-  res.render('index', {
-	  number: 42
-  });
+  res.render('index', {});
+});
+
+app.get('/play/:player/:number', function(req, res) {
+	number = req.params.number;
+	lastPlayer = req.params.player;
+	res.send('');
+});
+
+app.get('/player/last', function(req, res) {
+	res.send({
+		lastPlayer: lastPlayer
+	});
 });
 
 app.listen(8080);
